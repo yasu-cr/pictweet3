b9a4469a0799c47a207472b322965f1a2d2adc03
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2024-01-26 22:53:59 +0900

    nicknameとemailが空では登録できない時のテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000') # nicknameが空では登録できないテストコードを記述します
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it 'emailが空では登録できない' do
+      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')# emailが空では登録できないテストコードを記述します
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank") 
+    end 
+  end
 end
