4ac4c89933a1d9bd9693e645af785ef9004a975d
yasu-cr <150337096+yasu-cr@users.noreply.github.com>
2024-02-06 23:59:37 +0900

    単体テストを書き換えた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe User, type: :model do
     it 'nicknameが空では登録できない' do
       user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000') # nicknameが空では登録できないテストコードを記述します
       user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      expect(user.errors.full_messages).to include("ニックネームを入力してください")
     end
     it 'emailが空では登録できない' do
       user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')# emailが空では登録できないテストコードを記述します
       user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank") 
+      expect(user.errors.full_messages).to include("Eメールを入力してください") 
     end 
   end
 end
